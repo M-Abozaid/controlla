7d8789fa99e541375c21b691107c763241512874
Muhammed <aapozaid@gmail.com>
2020-03-01 01:20:00 +0200

    get active tabg

--- src/popup/Services/getCurrentUrl.ts
+++ src/popup/Services/getCurrentUrl.ts
@@ -1,4 +1,9 @@
 
-export function getCurrentUrl(){
+import chromep from 'chrome-promise';
+
+export async function getActiveTab():Promise<chrome.tabs.Tab>{
+    
+    const activeTab = await chromep.tabs.query({currentWindow: true, active: true})
+    return activeTab[0]
 
 }
