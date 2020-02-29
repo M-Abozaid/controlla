cfe28b90acdf36e595af41a726591472be9cd256
Muhammed <aapozaid@gmail.com>
2020-03-01 00:31:34 +0200

    new Rule type

---
+++ src/background/background.ts
@@ -0,0 +1,26 @@
+
+// const myRule: Rule = {
+//     matcher:{
+//         type: MatcherType.URL,
+//         value: 'something'
+//     },
+//     startTime:'something',
+//     endTime:'sdsd',
+//     daysOfWeek: [2]
+// }
+
+// interface Visit {
+
+// }
+
+// class Keeper{
+//     constructor(private storage, ){
+
+//         // this.getSettings
+//     }
+
+
+
+
+
+// }
--- src/popup/utils/index.ts
+++ src/popup/utils/index.ts
@@ -2,6 +2,6 @@
 @input: rule object
 @output: the title of the rule if url
 */
-export const getRuleTitle = rule => {
+export const getRuleTitle = (rule):RegExpMatchArray =>  {
   if (rule.url) return `${rule.url}`.match(/\w+/)
 }
